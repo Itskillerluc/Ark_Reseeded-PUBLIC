8426db6c901fd652713faf03647d38a58a58a8b6
HuskyTacodile <huskytakidile@gmail.com>
2022-07-23 20:13:52 -0500

    welp

--- src/main/java/com/huskytacodile/alternacraft/block/entity/DNAInsertionTableBlockEntity.java
+++ src/main/java/com/huskytacodile/alternacraft/block/entity/DNAInsertionTableBlockEntity.java
@@ -1,12 +1,7 @@
 package com.huskytacodile.alternacraft.block.entity;
 
-import com.huskytacodile.alternacraft.item.ModItems;
-import com.huskytacodile.alternacraft.item.custom.DNASyringeItem;
-import com.huskytacodile.alternacraft.recipe.FossilGrinderRecipe;
+import com.huskytacodile.alternacraft.recipe.DNAInsertionTableRecipe;
 import com.huskytacodile.alternacraft.screen.DNAInsertionTableMenu;
-import com.huskytacodile.alternacraft.screen.FossilGrinderMenu;
-import com.huskytacodile.alternacraft.util.DNATier;
-import com.huskytacodile.alternacraft.util.Dino;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
@@ -19,10 +14,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
@@ -35,18 +31,19 @@ import javax.annotation.Nonnull;
 import java.util.Optional;
 
 public class DNAInsertionTableBlockEntity extends BlockEntity implements MenuProvider {
-    private final ItemStackHandler itemHandler = new ItemStackHandler(2) {
+    private final ItemStackHandler itemHandler = new ItemStackHandler(4) {
         @Override
         protected void onContentsChanged(int slot) {
             setChanged();
         }
     };
-
     private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
 
     protected final ContainerData data;
     private int progress = 0;
-    private int maxProgress = 90;
+    private int maxProgress = 72;
+    private int fuelTime = 0;
+    private int maxFuelTime = 0;
 
     public DNAInsertionTableBlockEntity(BlockPos pWorldPosition, BlockState pBlockState) {
         super(ModBlockEntities.DNA_INSERTION_TABLE.get(), pWorldPosition, pBlockState);
@@ -55,6 +52,8 @@ public class DNAInsertionTableBlockEntity extends BlockEntity implements MenuPro
                 switch (index) {
                     case 0: return DNAInsertionTableBlockEntity.this.progress;
                     case 1: return DNAInsertionTableBlockEntity.this.maxProgress;
+                    case 2: return DNAInsertionTableBlockEntity.this.fuelTime;
+                    case 3: return DNAInsertionTableBlockEntity.this.maxFuelTime;
                     default: return 0;
                 }
             }
@@ -63,11 +62,13 @@ public class DNAInsertionTableBlockEntity extends BlockEntity implements MenuPro
                 switch(index) {
                     case 0: DNAInsertionTableBlockEntity.this.progress = value; break;
                     case 1: DNAInsertionTableBlockEntity.this.maxProgress = value; break;
+                    case 2: DNAInsertionTableBlockEntity.this.fuelTime = value; break;
+                    case 3: DNAInsertionTableBlockEntity.this.maxFuelTime = value; break;
                 }
             }
 
             public int getCount() {
-                return 2;
+                return 4;
             }
         };
     }
@@ -108,7 +109,9 @@ public class DNAInsertionTableBlockEntity extends BlockEntity implements MenuPro
     @Override
     protected void saveAdditional(@NotNull CompoundTag tag) {
         tag.put("inventory", itemHandler.serializeNBT());
-        tag.putInt("dna_insertion.progress", progress);
+        tag.putInt("insertion.progress", progress);
+        tag.putInt("insertion.fuelTime", fuelTime);
+        tag.putInt("insertion.maxFuelTime", maxFuelTime);
         super.saveAdditional(tag);
     }
 
@@ -116,7 +119,9 @@ public class DNAInsertionTableBlockEntity extends BlockEntity implements MenuPro
     public void load(CompoundTag nbt) {
         super.load(nbt);
         itemHandler.deserializeNBT(nbt.getCompound("inventory"));
-        progress = nbt.getInt("dna_insertion.progress");
+        progress = nbt.getInt("insertion.progress");
+        fuelTime = nbt.getInt("insertion.fuelTime");
+        maxFuelTime = nbt.getInt("insertion.maxFuelTime");
     }
 
     public void drops() {
@@ -128,12 +133,30 @@ public class DNAInsertionTableBlockEntity extends BlockEntity implements MenuPro
         Containers.dropContents(this.level, this.worldPosition, inventory);
     }
 
+    private void consumeFuel() {
+        if(!itemHandler.getStackInSlot(0).isEmpty()) {
+            this.fuelTime = ForgeHooks.getBurnTime(this.itemHandler.extractItem(0, 1, false),
+                    RecipeType.SMELTING);
+            this.maxFuelTime = this.fuelTime;
+        }
+    }
+
     public static void tick(Level pLevel, BlockPos pPos, BlockState pState, DNAInsertionTableBlockEntity pBlockEntity) {
+        if(isConsumingFuel(pBlockEntity)) {
+            pBlockEntity.fuelTime--;
+        }
+
         if(hasRecipe(pBlockEntity)) {
-            pBlockEntity.progress++;
-            setChanged(pLevel, pPos, pState);
-            if(pBlockEntity.progress > pBlockEntity.maxProgress) {
-                craftItem(pBlockEntity);
+            if(hasFuelInFuelSlot(pBlockEntity) && !isConsumingFuel(pBlockEntity)) {
+                pBlockEntity.consumeFuel();
+                setChanged(pLevel, pPos, pState);
+            }
+            if(isConsumingFuel(pBlockEntity)) {
+                pBlockEntity.progress++;
+                setChanged(pLevel, pPos, pState);
+                if(pBlockEntity.progress > pBlockEntity.maxProgress) {
+                    craftItem(pBlockEntity);
+                }
             }
         } else {
             pBlockEntity.resetProgress();
@@ -141,36 +164,57 @@ public class DNAInsertionTableBlockEntity extends BlockEntity implements MenuPro
         }
     }
 
+    private static boolean hasFuelInFuelSlot(DNAInsertionTableBlockEntity entity) {
+        return !entity.itemHandler.getStackInSlot(0).isEmpty();
+    }
+
+    private static boolean isConsumingFuel(DNAInsertionTableBlockEntity entity) {
+        return entity.fuelTime > 0;
+    }
+
     private static boolean hasRecipe(DNAInsertionTableBlockEntity entity) {
-        boolean hasEgg = entity.itemHandler.getStackInSlot(0).getItem() == Items.EGG ||
-                entity.itemHandler.getStackInSlot(0).getItem() == Items.TURTLE_EGG;
+        Level level = entity.level;
+        SimpleContainer inventory = new SimpleContainer(entity.itemHandler.getSlots());
+        for (int i = 0; i < entity.itemHandler.getSlots(); i++) {
+            inventory.setItem(i, entity.itemHandler.getStackInSlot(i));
+        }
 
-        boolean hasSyringe = entity.itemHandler.getStackInSlot(1).getItem() instanceof DNASyringeItem;
-        boolean isNotEmpty = entity.itemHandler.getStackInSlot(1).getItem() != ModItems.EMPTY_SYRINGE.get();
+        Optional<DNAInsertionTableRecipe> match = level.getRecipeManager()
+                .getRecipeFor(DNAInsertionTableRecipe.Type.INSTANCE, inventory, level);
 
-        return hasEgg & hasSyringe && isNotEmpty;
+        return match.isPresent() && canInsertAmountIntoOutputSlot(inventory)
+                && canInsertItemIntoOutputSlot(inventory, match.get().getResultItem());
     }
 
     private static void craftItem(DNAInsertionTableBlockEntity entity) {
-        entity.itemHandler.extractItem(0,1, false);
-        entity.itemHandler.extractItem(1,1, false);
-
-        entity.itemHandler.setStackInSlot(0, new ItemStack(ModItems.ACRO_SPAWN_EGG.get()));
-
-        entity.resetProgress();
-    }
-
-    private static int getNextFreeOutputSlot(SimpleContainer inventory) {
-        int toReturn = -1;
-        for(int i = 5; i >= 2; i--) {
-            toReturn = inventory.getItem(i).isEmpty() || (inventory.getItem(i).getMaxStackSize() < inventory.getItem(i).getCount() + 1)
-                    ? i : toReturn;
+        Level level = entity.level;
+        SimpleContainer inventory = new SimpleContainer(entity.itemHandler.getSlots());
+        for (int i = 0; i < entity.itemHandler.getSlots(); i++) {
+            inventory.setItem(i, entity.itemHandler.getStackInSlot(i));
         }
 
-        return toReturn;
+        Optional<DNAInsertionTableRecipe> match = level.getRecipeManager()
+                .getRecipeFor(DNAInsertionTableRecipe.Type.INSTANCE, inventory, level);
+
+        if(match.isPresent()) {
+            entity.itemHandler.extractItem(1,1, false);
+
+            entity.itemHandler.setStackInSlot(1, new ItemStack(match.get().getResultItem().getItem(),
+                    entity.itemHandler.getStackInSlot(1).getCount() + 1));
+
+            entity.resetProgress();
+        }
     }
 
     private void resetProgress() {
         this.progress = 0;
+    }
+
+    private static boolean canInsertItemIntoOutputSlot(SimpleContainer inventory, ItemStack output) {
+        return inventory.getItem(1).getItem() == output.getItem() || inventory.getItem(1).isEmpty();
+    }
+
+    private static boolean canInsertAmountIntoOutputSlot(SimpleContainer inventory) {
+        return inventory.getItem(1).getMaxStackSize() > inventory.getItem(1).getCount();
     }
 }
